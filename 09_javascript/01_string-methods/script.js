906ca4ed25177692049f0dbd00ed32090079a5ea
Christian Tisby <cduane82@hotmail.com>
2020-08-13 21:55:16 -0400

    adding javascript

---
+++ 09_javascript/01_string-methods/script.js
@@ -0,0 +1,18 @@
+let msg = "You are so grounded mr"
+
+msg.toUpperCase()
+console.log(msg)
+
+msg.toLowerCase()
+console.log(msg)
+
+let angry = "leAve Me AlOnE nOw"
+angry.toLowerCase() 
+console.log(angry)
+
+let color = "   purple  "
+color.trim()
+
+color.trim().toUpperCase()
+
+console.log(color)
