0ca176edccdb4a796380aef5653dc1e67147b67b
Christian Tisby <cduane82@hotmail.com>
2020-08-19 20:58:40 -0400

    updating javascript

---
+++ 09_javascript/07_conditional-statements/script.js
@@ -0,0 +1,17 @@
+let temp = 80
+
+if (temp > 70) {
+    console.log("Go to the beach")
+} else if (temp < 32) {
+    console.log("Go ice skating")
+} else {
+    console.log("Stay at home")
+}
+
+let favoriteDrink = "Coca Cola"
+
+if (favoriteDrink === "Coca Cola") {
+    console.log("Yessir!!!")
+} else {
+    console.log("I'm not thirsty")
+}
